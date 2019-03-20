14c975c37b8745b94647bdadf97afad1093e0beb
Anita Jarikre <anitajarikre55@gmail.com>
2019-03-20 13:47:21 +0100

    Designed Database

---
+++ awlc-online2019.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.6deb5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Mar 20, 2019 at 01:45 PM
+-- Server version: 5.7.25-0ubuntu0.18.04.2
+-- PHP Version: 7.2.15-0ubuntu0.18.04.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `AWLCRwanda2019`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `awlc-online2019`
+--
+
+CREATE TABLE `awlc-online2019` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `firstName` text NOT NULL,
+  `lastName` text NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `phone` varchar(15) NOT NULL,
+  `country` text NOT NULL,
+  `occupation` text NOT NULL,
+  `organisation` text NOT NULL,
+  `member` text NOT NULL,
+  `referringChannel` text NOT NULL,
+  `firstConference` text NOT NULL,
+  `referrer` date NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `paid` enum('yes','no') DEFAULT NULL,
+  `paid_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `awlc-online2019`
+--
+ALTER TABLE `awlc-online2019`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `awlc-online2019`
+--
+ALTER TABLE `awlc-online2019`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
