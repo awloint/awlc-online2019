ff5c9e9ea5d86db6590f895bf179369441a71485
Benson Imoh, ST <benson@stbensonimoh.com>
2019-03-22 00:10:00 +0100

    Fix Bug preventing notfication from going out

--- scripts/verify.php
+++ scripts/verify.php
@@ -52,7 +52,7 @@ if ('success' == $trx->data->status) {
         if ($db->userExists($email, "awlcrwandavirtual")) {
             // Select the user
             $result = $db->userSelect($email, "awlcrwandavirtual");
-            print_r($result);
+
             // get the phone number
             foreach ($result as $key => $value) {
                 ${$key} = $value;
